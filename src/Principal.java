c2f5aab81692a1605bd05fe79a06ce0378d38cc1
alanecosta <alanecosta@gmail.com>
2025-01-05 17:52:15 -0300

    Criação das classes Titulo e Series. Utilização de herança de titulo para filmes e series

--- src/Principal.java
+++ src/Principal.java
@@ -1,4 +1,5 @@
 import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
 
 public class Principal {
     public static void main(String[] args) {
@@ -13,5 +14,12 @@ public class Principal {
         meuFilme.avalia(9.7);
         System.out.println("Total das avaliações: " + meuFilme.getTotalDeAvaliacoes());
         System.out.println(meuFilme.pegaMedia());
+
+        Serie Friends = new Serie();
+        Friends.setNome("Friends");
+        Friends.setAnoDeLancamento(1994);
+        Friends.setDuracaoEmMInutos(5000);
+        Friends.setTemporadas(10);
+        Friends.setEpisodiosPorTemporada(10);
     }
 }
