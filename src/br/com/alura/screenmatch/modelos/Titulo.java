9d7ccebba4f99196fdac8b408976764a6b46aa41
alanecosta <alanecosta@gmail.com>
2025-01-10 20:30:10 -0300

    Adeed:  conversão de um json em um objeto Java

--- src/br/com/alura/screenmatch/modelos/Titulo.java
+++ src/br/com/alura/screenmatch/modelos/Titulo.java
@@ -19,6 +19,12 @@ public class Titulo implements Comparable<Titulo> {
         this.anoDeLancamento = anoDeLancamento;
     }
 
+    public Titulo(TituloOmdb meuTituloOmdb) {
+        this.nome = meuTituloOmdb.title();
+        this.anoDeLancamento = Integer.valueOf(meuTituloOmdb.year());
+        this.duracaoEmMInutos = Integer.valueOf(meuTituloOmdb.runtime().substring(0, 2));
+    }
+
     public String getNome() {
         return nome;
     }
@@ -69,7 +75,7 @@ public class Titulo implements Comparable<Titulo> {
     @Override
     public String toString() {
         return  "nome='" + nome + '\'' +
-                ", anoDeLancamento=" + anoDeLancamento +
-                '}';
+                ", anoDeLancamento=" + anoDeLancamento + "," +
+                "duração " + duracaoEmMInutos + " minutos";
     }
 }
